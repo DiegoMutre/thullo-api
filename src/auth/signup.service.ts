6cf2fe83b221505abb1dc9f64393d676dd42e044
DiegoMutre <tysonmutre10@gmail.com>
2023-09-12 19:45:51 -0500

    * Finish `createUser` method
    Now it can be able to create users and add it to the DB

--- src/auth/signup.service.ts
+++ src/auth/signup.service.ts
@@ -1,14 +1,78 @@
 import { Prisma } from '@prisma/client';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
 
 @Injectable()
 export class SignupService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private configService: ConfigService,
+  ) {}
 
-  async createUser(userData: Prisma.UserCreateInput) {
-    await this.prismaService.user.create({
-      data: userData,
+  async createUser(data: Prisma.UserCreateInput, res: Response) {
+    // Hash the password
+    const password = await bcrypt.hash(data.password, 10);
+
+    // Save the encrypted password
+    const userCreated = await this.prismaService.user.create({
+      data: {
+        ...data,
+        password,
+      },
+      // Select this data to be returned
+      select: {
+        id: true,
+        email: true,
+        username: true,
+      },
     });
+
+    // Create access token
+    const accessToken = jwt.sign(
+      userCreated,
+      this.configService.get('SECRET_KEY'),
+      {
+        expiresIn: '5m', // Expires in five minutes
+      },
+    );
+
+    // Create refresh token
+    const refreshToken = jwt.sign(
+      userCreated,
+      this.configService.get('SECRET_KEY'),
+      {
+        expiresIn: '30 days',
+      },
+    );
+
+    // Save the refresh token in the user collection
+    // Yeah, I know that this can be better because I'm querying the database again which doesn't
+    // seem to be the most optimal way
+    await this.prismaService.user.update({
+      where: {
+        id: userCreated.id,
+      },
+      data: {
+        refreshToken,
+      },
+    });
+
+    // Set a http-only cookie with the refresh token
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      // It expires but in a date far in the future, though it can be unnecessary because
+      // the refresh token expires in 30 days
+      expires: new Date(2147483647000),
+    });
+
+    // Return access token
+    return {
+      accessToken,
+    };
   }
 }
