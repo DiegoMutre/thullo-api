2bf443282dde74a894515638eb053db2595f3e9d
DiegoMutre <tysonmutre10@gmail.com>
2023-09-12 20:36:08 -0500

    * Configure cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable reading/setting cookies
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
+
   app.enableCors({
     origin: process.env.CLIENT_URL, // Reads from the environment variable
+    credentials: true,
   });
   await app.listen(4000);
 }
