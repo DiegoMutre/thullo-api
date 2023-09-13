2034370577fe386b5cb9bab8cdb46c0754fa0d9e
DiegoMutre <tysonmutre10@gmail.com>
2023-09-12 19:43:17 -0500

    * Import the `ConfigModule` and make it global
    Now the .env variables can be accessed globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { SignupModule } from 'src/auth/signup.module';
 
 @Module({
-  imports: [SignupModule, ConfigModule.forRoot()],
+  imports: [
+    SignupModule,
+    ConfigModule.forRoot({
+      // It can be accessed as a service in any module
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
